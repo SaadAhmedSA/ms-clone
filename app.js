78f6879a43f5822dd8f90456df33da1daf223f76
saad ahmed <ss5099903@gmail.com>
2024-05-31 19:58:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function bold(){
+    var text = document.getElementById("text").style.fontWeight="bold"
+    
+}
+function ital(){
+    var text = document.getElementById("text").style.fontStyle="italic"
+
+}
+function under(){
+    var text = document.getElementById("text").style.textDecoration="underline"
+
+}
+function font(){
+    var font=16
+font++
+
+    var text = document.getElementById("text").style.fontSize=`${font}px`
+
+}
+function font2(){
+    var font=16
+font--
+
+    var text = document.getElementById("text").style.fontSize=`${font}px`
+
+}
+
